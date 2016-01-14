830d3909abb0a685e3f11dbb54df5e6947db8f50
parkjunhyo <junhyo.luck@gmail.com>
2016-01-14 13:42:49 -0500

    update

--- checkpoint_utility/renew_security_objects.py
+++ checkpoint_utility/renew_security_objects.py
@@ -19,7 +19,13 @@ output_data_list = [output_src_data, output_dst_data, output_port_data]
 random.seed(time.clock())
 
 
-def lookup_object(policy_no_in, policy_object_in):
+def lookup_object(policy_no_in, policy_object_in, output_file_name):
+
+   if policy_object_in == "Any\n":
+     f = open(output_file_name,"a")
+     f.write("\t".join([policy_no_in, "0.0.0.0/0"]))
+     f.close()
+
    object_name = policy_object_in.rstrip()
    random_name = str(random.random())
    bash_result = random_name.split(".")[1]
@@ -43,10 +49,26 @@ def lookup_object(policy_no_in, policy_object_in):
       matched_comments = all_info_list[8]
 
       if matched_name == object_name and matched_type == "Host Node":
-        print "\t".join([policy_no_in, matched_ip+"/32"])
+        f = open(output_file_name,"a")
+        f.write("\t".join([policy_no_in, matched_ip+"/32"]))
+        f.close()
+
+      if matched_name == object_name and (matched_type == "Check Point Host" or matched_type == "Gateway Cluster" or matched_type =="Check Point Gateway" or matched_type =="Cluster Member"):
+        if matched_mask != "-":
+          random_no = str(random.random())
+          r_file = random_no.split(".")[1]
+          os.system("./ip_calc.sh %s %s %s" % (matched_ip, matched_mask, r_file))
+          f = open(r_file,"r")
+          renew_matched_info = f.readlines()
+          f.close()
+          os.system("rm -rf ./%s" % (r_file))
+          renew_subnet_size = renew_matched_info[0].rstrip().split("/")[1]
+          f = open(output_file_name,"a")
+          f.write("\t".join([policy_no_in,matched_ip+"/"+renew_subnet_size]))
+          f.close()
 
       if matched_name == object_name and matched_type == "Group":
-        lookup_object(policy_no_in, matched_member) 
+        lookup_object(policy_no_in, matched_member, output_file_name) 
 
       if matched_name == object_name and matched_type == "Network":
         random_no = str(random.random())
@@ -56,7 +78,9 @@ def lookup_object(policy_no_in, policy_object_in):
         renew_matched_info = f.readlines()
         f.close()
         os.system("rm -rf ./%s" % (r_file))
-        print "\t".join([policy_no_in,renew_matched_info[0].rstrip()])
+        f = open(output_file_name,"a")
+        f.write("\t".join([policy_no_in,renew_matched_info[0].rstrip()]))
+        f.close()
 
       if matched_name == object_name and matched_type == "Address Range":
         first_ip = ip_address(str(matched_ip.split()[0]))
@@ -64,10 +88,11 @@ def lookup_object(policy_no_in, policy_object_in):
         range_ip_list = []
         while first_ip <= last_ip:
            range_ip_list.append(str(first_ip))
-           print object_name
-           print "\t".join([policy_no_in, str(first_ip)])
+           f = open(output_file_name,"a")
+           f.write("\t".join([policy_no_in, str(first_ip)+"/32"]))
+           f.close()
            first_ip = first_ip + 1
-
+   
 
 list_index = 0
 for origin_data in origin_data_list:
@@ -76,14 +101,16 @@ for origin_data in origin_data_list:
    read_contents = f.readlines()
    f.close()
 
+   output_file_name = output_data_list[list_index]
+   f = open(output_file_name,"w")
+   f.close()
    for read_content in read_contents:
-
       read_content_list = read_content.split("\t")
-
       policy_no = read_content_list[0]
       policy_object = read_content_list[1]
+      lookup_object(policy_no,policy_object,output_file_name)
+      print "policy number [ %s ] has been done" % (policy_no)
 
-
-      lookup_object(policy_no,policy_object)
+   list_index = list_index + 1
 
       
