bf13de7a7f8951eafe59bd7ac98881e15d72d0f1
parkjunhyo <junhyo.luck@gmail.com>
2016-01-14 13:43:35 -0500

    update

--- checkpoint_utility/renew_security_objects.py
+++ checkpoint_utility/renew_security_objects.py
@@ -12,8 +12,7 @@ output_dst_data = "./destination.renew"
 output_port_data = "./service.renew"
 
 
-# origin_data_list = [origin_src_data, origin_dst_data, origin_port_data]
-origin_data_list = [origin_src_data]
+origin_data_list = [origin_src_data, origin_dst_data, origin_port_data]
 output_data_list = [output_src_data, output_dst_data, output_port_data]
 
 random.seed(time.clock())
