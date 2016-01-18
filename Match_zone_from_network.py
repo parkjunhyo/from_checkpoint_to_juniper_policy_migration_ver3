6baf6e7767d73e6ba3350ae84853cbb3ee68206c
parkjunhyo <junhyo.luck@gmail.com>
2016-01-18 04:01:12 -0500

    update

--- Match_zone_from_network.py
+++ Match_zone_from_network.py
@@ -6,11 +6,17 @@ from Utility_function import Utility_function
 from utils import *
 import ipcalc
 
-class Policy_source_ip_address_renew:
+class Match_zone_from_network:
  
   default_zone_name = "" 
-  read_files_name = ["./source.renew"]
+  read_files_name = ["./source.renew","destination.renew"]
+  output_files_name = ["./source.zonematch","destination.zonematch"]
   database_network_zone = {}
+
+  def writeOutput(self, file_name, input_data):
+     f = open(file_name,"a")
+     f.write(input_data)
+     f.close()
 
   def Start(self):
 
@@ -18,9 +24,15 @@ class Policy_source_ip_address_renew:
      self.default_zone_name = findDefaultzone(Routing_information.zone_name, Routing_information.network_mask)
 
      # read file 
+     file_index = 0
      for read_file_name in self.read_files_name:
         f = open(read_file_name,"r")
         contents_in_read_file = f.readlines()
+        f.close()
+
+        # output file initailzation
+        out_file_name = self.output_files_name[file_index]
+        f = open(out_file_name,"w")
         f.close()
  
         for content_in_read_file in contents_in_read_file:
@@ -33,16 +45,15 @@ class Policy_source_ip_address_renew:
              # default zone
              if "0.0.0.0/0" == policy_object:
                self.database_network_zone[policy_object] = self.default_zone_name
-               print [policy_number, self.default_zone_name, policy_object+"\n"]
+               combination = [ policy_number, self.default_zone_name, policy_object ]
+               writeOutput(out_file_name, combination)
                continue
+
              # find zone name.. ratating all routing information
-             
              compare_subnet_size = "0"
              match_status = False
-             match_zone_name = ""
              for _zone_name_ in Routing_information.zone_name:
                 for _network_mask_ in Routing_information.network_mask[_zone_name_]:
-
                    if "0.0.0.0/0" == _network_mask_:
                      continue
                    # target information
@@ -54,20 +65,28 @@ class Policy_source_ip_address_renew:
                          compare_subnet_size = _subnet_size_
                          self.database_network_zone[policy_object] = _zone_name_
                          match_status = True
-                         match_zone_name = _zone_name_ 
+                   else:
+                     if _ip_address_ in ipcalc.Network(policy_object):
+                       combination = [ policy_number, _zone_name_, _network_mask_ ]
+                       writeOutput(out_file_name, combination)
             
              # Not matched case, it will be default gateway zone. 
              if not match_status:
                self.database_network_zone[policy_object] = self.default_zone_name
-               print [policy_number, self.default_zone_name, policy_object+"\n"]
+               combination = [ policy_number, self.default_zone_name, policy_object ]
+               writeOutput(out_file_name, combination)
              else:
-               print [policy_number, self.database_network_zone[policy_object], policy_object+"\n"]
+               combination = [ policy_number, self.database_network_zone[policy_object], policy_object ]
+               writeOutput(out_file_name, combination)
 
            # searching database and fine zone according to network
            else:
-             print ["D", policy_number, self.database_network_zone[policy_object], policy_object+"\n"]
+             combination = [ policy_number, self.database_network_zone[policy_object], policy_object ]
+             writeOutput(out_file_name, combination)
+     #
+     file_index = file_index + 1
 
 
 if __name__=="__main__":
-  root_process = Policy_source_ip_address_renew()
+  root_process = Match_zone_from_network()
   root_process.Start()
