22e9edd2ecd5b1c70517c2e15f3ad2dddbb032ae
parkjunhyo <junhyo.luck@gmail.com>
2016-02-02 13:55:28 +0900

    update

---
+++ upload_html.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+html_dir="/var/www/html/juniper"
+
+function moving {
+ if [ -f $1 ]
+ then
+  mv $1 $2
+ fi
+}
+
+if [ -d $html_dir ]
+then
+ rm -rf $html_dir
+fi
+
+mkdir $html_dir
+chmod 777 $html_dir
+
+file_name="./juniper_type_result.txt"
+moving $file_name $html_dir
+
+file_name="./text_type_result.txt"
+moving $file_name $html_dir
